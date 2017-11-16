71800e0ed9ac3e18278d59568d57dbeca4eb199d
Mike Hemberger <mike@thestizmedia.com>
2017-11-16 18:17:31 -0500

    v bump

--- wampum-forms.php
+++ wampum-forms.php
@@ -16,7 +16,7 @@
  * License:            GPL-2.0+
  * License URI:        http://www.gnu.org/licenses/gpl-2.0.txt
  *
- * Version:            1.2.4
+ * Version:            1.3.0
  *
  * GitHub Plugin URI:  https://github.com/bizbudding/wampum-forms
  * GitHub Branch:      master
@@ -123,7 +123,7 @@ final class Wampum_Forms_Setup {
 	private function setup_constants() {
 		// Plugin version.
 		if ( ! defined( 'WAMPUM_FORMS_VERSION' ) ) {
-			define( 'WAMPUM_FORMS_VERSION', '1.2.4' );
+			define( 'WAMPUM_FORMS_VERSION', '1.3.0' );
 		}
 		// Plugin Folder Path.
 		if ( ! defined( 'WAMPUM_FORMS_PLUGIN_DIR' ) ) {
